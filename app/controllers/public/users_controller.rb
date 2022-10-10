a8a3d2484925e9812bf11c2f26c860f913c43f44
crack-von-wiegenlied <110370914+crack-von-wiegenlied@users.noreply.github.com>
2022-10-10 08:56:37 +0000

    [add]ユーザー情報編集機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,11 +4,26 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
   def unsubscribe
   end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :introduction, :image, :email)
+    end
+
 end
