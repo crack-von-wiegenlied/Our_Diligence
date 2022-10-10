545434a12dde9e32f7ea75fd775aff90359ad842
crack-von-wiegenlied <110370914+crack-von-wiegenlied@users.noreply.github.com>
2022-10-10 06:41:37 +0000

    [add]仮ヘッダー設置

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
 
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about',as:'about'
+    resources :users, only: [:show, :edit, :update, :index]
+    get 'users/unsubscribe'
+    patch 'users/withdraw'
+  end
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
